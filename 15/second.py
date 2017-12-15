d0ec69b220ce99c4efefa7c4b3dbe1c87ad132bd
Purple Myst <PurpleMyst@users.noreply.github.com>
2017-12-15 15:36:27 +0100

    Add Day 15

---
+++ 15/second.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+
+def generator_a(starting_value):
+    current = starting_value
+
+    while True:
+        current = (current * 16807) % 2147483647
+
+        if current % 4 == 0:
+            yield current
+
+
+def generator_b(starting_value):
+    current = starting_value
+
+    while True:
+        current = (current * 48271) % 2147483647
+
+        if current % 8 == 0:
+            yield current
+
+
+def main():
+    a = generator_a(634)
+    b = generator_b(301)
+    total = 0
+
+    for _ in range(5 * 10 ** 6):
+        current_a = next(a) & 0xFFFF
+        current_b = next(b) & 0xFFFF
+
+        if current_a == current_b:
+            total += 1
+
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
